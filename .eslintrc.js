e20d92748f4a3032d3b68858c3fdc19e40f1fb72
KevenMor <keven.km.mor@gmail.com>
2025-06-23 14:43:46 -0300

    fix: remover .eslintrc.js problematico para usar configuracao do package.json

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['react-app'],
-  rules: {
-    'no-unused-vars': 'off', // Desabilitar regra de variáveis não utilizadas
-    '@typescript-eslint/no-unused-vars': 'off' // Desabilitar regra para TypeScript também
-  }
-}; 
