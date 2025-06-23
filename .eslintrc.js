35e584a8be87e712ccd9cc0d6924310053d094cf
KevenMor <keven.km.mor@gmail.com>
2025-06-23 14:50:14 -0300

    desabilitar eslint durante build e atualizar railwayignore

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// ESLint configuration
+// A configuração principal está no package.json na seção "eslintConfig"
+// Este arquivo existe apenas para evitar erros de build no Railway
+
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ]
+}; 
